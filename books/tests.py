3f25a043c03100b71a4f4b621abfaa458803ab21
danny <jossuedaniel@outlook.com>
2023-08-29 15:13:10 -0500

    Revisiones como claves foráneas C12

--- books/tests.py
+++ books/tests.py
@@ -1,17 +1,30 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-from .models import Book
+from .models import Book, Review
 
 
 # Create your tests here.
 class BookTest(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="reviewuser",
+            email="reviewuser@email.com",
+            password="testpass123",
+        )
+
         cls.book = Book.objects.create(
             title="Harry Potter",
             author="J. K. Rowling",
             price="25.00",
+        )
+
+        cls.review = Review.objects.create(
+            book=cls.book,
+            author=cls.user,
+            review="Una excelente revisión",
         )
 
     def test_book_listing(self):
@@ -31,4 +44,5 @@ class BookTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "Una excelente revisión")
         self.assertTemplateUsed(response, "books/book_detail.html")
